c3ad347fc12871b491c244caa7d650d694b46865
torz <torz@gmx.com>
2019-01-27 21:30:42 +0000

    missing in action

--- aws/remote_state/iam.tf
+++ aws/remote_state/iam.tf
@@ -2,6 +2,10 @@ data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "assume" {
   statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
     principals {
       type = "AWS"
 
