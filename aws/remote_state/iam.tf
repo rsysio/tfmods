99bb274471d6add68cbd2d4313d5344bc8fd634e
torz <torz@gmx.com>
2019-01-27 21:12:51 +0000

    missing policy

--- aws/remote_state/iam.tf
+++ aws/remote_state/iam.tf
@@ -33,7 +33,11 @@ data "aws_iam_policy_document" "tfstate" {
   }
 }
 
+resource "aws_iam_policy" "tfstate" {
+  policy = "${data.aws_iam_policy_document.tfstate.json}"
+}
+
 resource "aws_iam_role" "tfstate" {
   name               = "tfstate"
-  assume_role_policy = "${data.aws_iam_policy_document.tfstate.json}"
+  assume_role_policy = "${aws_iam_policy.tfstate.arn}"
 }
