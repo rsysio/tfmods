df69931faf35cddb7dc84562c34f132d187628da
torz <torz@gmx.com>
2019-11-15 21:33:24 +0000

    Tf012/remote state (#1)
    * update s3 backend to 0.12

--- aws/remote_state/_input.tf
+++ aws/remote_state/_input.tf
@@ -1,5 +1,5 @@
 variable tags {
-  type        = "map"
+  type        = map(string)
   description = "Tags to set on S3 backend resources"
 
   default = {
@@ -8,46 +8,28 @@ variable tags {
 }
 
 variable "bucket_name_prefix" {
-  type        = "string"
+  type        = string
   description = "account alias set in IAM"
 }
 
 variable "backend_region" {
-  type        = "string"
+  type        = string
   description = "region for the remote state"
   default     = "us-east-1"
 }
 
 variable "dynamodb_table_name" {
-  type        = "string"
+  type        = string
   description = "http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html"
   default     = "terraform_statelock"
 }
 
 variable "dynamodb_read_capacity" {
   description = "Dynamodb throughput capacity for reads"
-  default     = 5
+  default     = 1
 }
 
 variable "dynamodb_write_capacity" {
   description = "Dynamodb throughput capacity for writes"
   default     = 1
-}
-
-variable "param_tfstate_dynamodb" {
-  type        = "string"
-  description = "Parameter store key for dynamodb table name"
-  default     = "/terraform/backend/dynamodb_table"
-}
-
-variable "param_tfstate_s3_bucket" {
-  type        = "string"
-  description = "Parameter store key for S3 bucket name"
-  default     = "/terraform/backend/bucket"
-}
-
-variable "param_tfstate_region" {
-  type        = "string"
-  description = "Parameter store key for backend region"
-  default     = "/terraform/backend/region"
 }
--- aws/remote_state/_output.tf
+++ aws/remote_state/_output.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_arn" {
-  value = "${aws_s3_bucket.remote_state.arn}"
+  value = aws_s3_bucket.remote_state.arn
 }
 
 output "dynamodb_table_arn" {
-  value = "${aws_dynamodb_table.terraform_statelock.arn}"
+  value = aws_dynamodb_table.terraform_statelock.arn
 }
