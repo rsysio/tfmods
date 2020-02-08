71bf129f05f3519bf46c391ef9a32f0241ac2e7e
torz <richard.szabo@viooh.com>
2020-02-08 16:51:43 +0000

    count list provided

--- aws/static_site/acm.tf
+++ aws/static_site/acm.tf
@@ -12,7 +12,7 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count = length(aws_acm_certificate.cert.domain_validation_options)
+  count = length(concat([var.fqdn], var.aliases))
 
   name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
   type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
