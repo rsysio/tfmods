d55a56887f8906c32cf9427275096d0cd7f925af
Richard Szabo <torz@rsys.io>
2022-04-09 12:06:10 +0100

    more hcl updates on s3 mod

--- aws/remote_state/s3.tf
+++ aws/remote_state/s3.tf
@@ -1,21 +1,23 @@
 resource "aws_s3_bucket" "remote_state" {
   bucket = "${var.bucket_name_prefix}-tfstate"
-  acl    = "private"
-
-  # statefile curruption safeguard
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 
   tags = var.tags
 }
 
+resource "aws_s3_bucket_acl" "remote_state" {
+  bucket = aws_s3_bucket.remote_state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "remote_state" {
+  bucket = aws_s3_bucket.remote_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "remote_state" {
-  bucket = aws_s3_bucket.remote_state.bucket
+  bucket = aws_s3_bucket.remote_state.id
 
   rule {
     apply_server_side_encryption_by_default {
